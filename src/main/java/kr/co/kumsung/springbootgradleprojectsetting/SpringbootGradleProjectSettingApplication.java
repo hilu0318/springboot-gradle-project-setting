7878d117c6cf84b90df6be45de5333591f3cc023
byunghakjang1230 <byunghakjang1230@gmail.com>
2021-10-22 17:20:43 +0900

    create project

---
+++ src/main/java/kr/co/kumsung/springbootgradleprojectsetting/SpringbootGradleProjectSettingApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kumsung.springbootgradleprojectsetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGradleProjectSettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGradleProjectSettingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/kumsung/springbootgradleprojectsetting/SpringbootGradleProjectSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kumsung.springbootgradleprojectsetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGradleProjectSettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
